e01e4f9287a3be51adefb24335fbb8496af63a9c
Mar3e <bintaleb.mar@gmail.com>
2025-05-07 17:04:10 +0300

    need custom dir only for neovim chad config

---
+++ custom/configs/dap.lua
@@ -0,0 +1,21 @@
+local dap = require "dap"
+
+dap.adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = { "~/Projects/debuggers/js-debug/src/dapDebugServer.js", "${port}" },
+  },
+}
+
+dap.configurations.javascript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+}
